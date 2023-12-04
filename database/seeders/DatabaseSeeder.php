99c7d8c3731dc5f10be7cc5dc4b88eb76e77ff4c
younesghu <younesghu44@gmail.com>
2023-12-04 15:55:32 +0100

    blog factoring

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Blog;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -10,9 +11,12 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(1)->create();
+        Blog::factory(6)->create(['user_id' => 1]);
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',

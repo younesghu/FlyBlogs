00ccd72b598c9074901ee647d82e735732174722
younesghu <younesghu44@gmail.com>
2024-03-03 18:39:51 +0100

    fixed create's background

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -43,9 +43,9 @@
                 </svg>
             </button>
             <div class="pl-4 items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-                <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700">
                     <li>
-                        <a href="/blogs/create" class="flex items-center py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                        <a href="/blogs/create" class="flex items-center py-2 px-3 text-gray-900 rounded md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 bg-transparent dark:hover:text-white dark:border-gray-700">
                             <span class="mr-2">Create</span>
                             <svg class="w-4 h-4 transform md:p-0 dark:border-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M9.546.5a9.5 9.5 0 1 0 9.5 9.5 9.51 9.51 0 0 0-9.5-9.5ZM13.788 11h-3.242v3.242a1 1 0 1 1-2 0V11H5.304a1 1 0 0 1 0-2h3.242V5.758a1 1 0 0 1 2 0V9h3.242a1 1 0 1 1 0 2Z"/>
